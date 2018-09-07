c6d1ca14c4a6a66d46f8b7835bad6c340033d7f2
殷俊杰 <247385709@qq.com>
2018-09-07 16:02:33 +0800

    validation

---
+++ validation/src/main/java/com/yjj/ValidationApp.java
@@ -0,0 +1,17 @@
+package com.yjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: yinjunjie
+ * @CreateDate: 2018/9/3 16:44
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class ValidationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationApp.class,args);
+    }
+}
