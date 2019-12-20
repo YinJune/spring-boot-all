ed172765508c347f4c9e5c5ead13adb6f2debcfe
oe_yinjunjie <123>
2019-12-20 16:12:45 +0800

    整合redis

---
+++ spring-boot-redis/src/main/java/com/yjj/redis/RedisApp.java
@@ -0,0 +1,16 @@
+package com.yjj.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Yin JunJie
+ * @Date: 2019/12/20 10:45
+ * @Description:
+ */
+@SpringBootApplication
+public class RedisApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApp.class,args);
+    }
+}
